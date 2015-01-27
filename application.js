10d867bf4314427224f8b1e31354ddadbc0d0f32
dvberkel <daan.v.berkel.1980@gmail.com>
2015-01-27 17:51:11 +0100

    use flood filler

--- application.js
+++ application.js
@@ -7,7 +7,8 @@
 
 	var canvas = document.getElementById('pixel-editor');
 	var view = new sprite.editor.View(model, canvas);
-	var controller = sprite.editor.controllerFor(model, view, canvas);
+	var controller = sprite.flood.fill.controllerFor(model, view, canvas);
+
 
 	canvas.addEventListener('mousedown', controller.startDrawing.bind(controller));
 	canvas.addEventListener('mouseup', controller.stopDrawing.bind(controller));
