3f33121219abc743780193f779efa2ac1ac33a54
dvberkel <daan.v.berkel.1980@gmail.com>
2015-01-27 21:28:53 +0100

    recursive flood fill algorithm

--- cois-flood-fill.js
+++ cois-flood-fill.js
@@ -3,56 +3,29 @@
     var flood = sprite.flood = {};
     var fill = flood.fill = {};
 
-    var Point = function(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    Point.prototype.neighbours = function(){
-        return [
-            new Point(this.x - 1, this.y),
-            new Point(this.x + 1, this.y),
-            new Point(this.x, this.y - 1),
-            new Point(this.x, this.y + 1)
-        ];
-    }
-    Point.prototype.admissable = function(model){
-        return this.x >= 0 && this.x < model.columns && this.y >= 0 && this.y < model.rows;
-    }
+    function floodAlgorithm(model, x, y, originalColor){
+        model.paintPixel(x, y, false);
+        if (x - 1 >= 0 && model.colorAt(x - 1, y) === originalColor) {
+            floodAlgorithm(model, x - 1, y, originalColor);
+        }
+        if (x + 1 < model.columns && model.colorAt(x + 1, y) === originalColor) {
+            floodAlgorithm(model, x + 1, y, originalColor);
+        }
+        if (y - 1 >= 0 && model.colorAt(x, y - 1) === originalColor) {
+            floodAlgorithm(model, x, y - 1, originalColor);
+        }
+        if (y + 1 < model.rows && model.colorAt(x, y + 1) === originalColor) {
+            floodAlgorithm(model, x, y + 1, originalColor);
+        }
 
-    var History = function(){
-        this.visitedPoints = {};
-    }
-    History.prototype.visited = function(point){
-        return this.visitedPoints[point.x] && this.visitedPoints[point.x][point.y];
-    }
-    History.prototype.visit = function(point){
-        this.visitedPoints[point.x] = this.visitedPoints[point.x] || {};
-        this.visitedPoints[point.x][point.y] = true;
     }
 
     var Flooder = fill.Flooder = function(model){
         this.model = model;
     };
     Flooder.prototype.flood = function(x, y) {
-        var toVisit = [new Point(x, y)];
-        var toVisitIndex = 0;
-        var history = new History();
-        var originalColor = this.model.colorAt(x, y);
-        while (toVisitIndex < toVisit.length) {
-            var point = toVisit[toVisitIndex++];
-            if (!history.visited(point)) {
-                history.visit(point);
-                point.neighbours().filter(function(neighbour){
-                    return neighbour.admissable(this.model) &&
-                        this.model.colorAt(neighbour.x, neighbour.y) === originalColor;
-                }.bind(this)).forEach(function(neighbour){
-                    toVisit.push(neighbour);
-                });
-            }
-        }
-        toVisit.forEach(function(point){
-            this.model.paintPixel(point.x, point.y);
-        }.bind(this));
+        floodAlgorithm(this.model, x, y, this.model.colorAt(x, y));
+        this.model.refresh();
     }
 
     var Controller = function(model, view, canvas){
@@ -75,13 +48,7 @@
         var column = Math.floor(x/this.view.pixelSize);
         var row = Math.floor(y/this.view.pixelSize);
 
-        try {
-            this.flooder.flood(column, row);
-        } catch(e) {
-            if (!e.message.match(/^Not within bounds/)) {
-                throw e;
-            }
-        }
+        this.flooder.flood(column, row);
     };
 
     fill.controllerFor = function(model, view, canvas){
